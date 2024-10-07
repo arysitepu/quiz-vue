ea198d81b020117a026741746486b348f0c67501
ari.rizkita <ari.rizkita@praweda.id>
2024-10-07 17:14:38 +0700

    upload sudah sampai pembuatan card quiz

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <main>
+    <header>
+      <h1 id="title">Quizz Vue</h1>
+      <input type="text" id="search-input">
+    </header>
+    <section id="quiz-container">
+      <div class="card">
+        <img src="https://images.unsplash.com/photo-1607799279861-4dd421887fb3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        <div class="card-body">
+          <h2>Programming</h2>
+          <p>2 Question</p>
+        </div>
+      </div>
+      <div class="card">
+        <img src="https://images.unsplash.com/photo-1607799279861-4dd421887fb3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+        <div class="card-body">
+          <h2>Programming</h2>
+          <p>2 Question</p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<style scope>
+main{
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+header{
+  margin-top: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#title{
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+#search-input{
+  border: none;
+  background-color: #c9c9c9a9;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+#quiz-container{
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 20px;
+}
+
+.card{
+  width: 270px;
+  margin-right: 30px;
+  margin-bottom:30px;
+  border-radius: 5px;
+  overflow:hidden;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+}
+
+.card img{
+  width: 100%;
+  height:200px;
+  object-fit:cover;
+  margin: 0;
+}
+
+.card-body{
+  padding: 0 15px;
+}
+
+.card-body h2{
+  font-weight: bold;
+
+}
+</style>
